eb47c7483183334f7f3949ff52e0f8ea651838df
Aywazyan <drackula333@gmail.com>
2024-06-02 18:38:35 +0400

    Added folder 4

---
+++ Homework_4/lesson.py
@@ -0,0 +1,64 @@
+# list_1 = [1,2,3,4,5,6,7,8,9,10]
+# list_2 = []
+
+# for i in list_1:
+#     if i % 2 == 1:
+#         list_2.append(i)
+
+# print(len(list_2))
+
+
+
+
+# list_vowel = "Hello world and galaxy" 
+
+# vowel = ["a", "e", "i", "o", "u", "y"]
+
+# res = []
+
+# for x in vowel:
+#     if x in list_vowel:
+#         res.append(x)
+# print(len(res))
+
+
+# word = "Hello"
+
+# word_list = ["Hello world", "World", "This", "Is", "List"]
+
+# target_list = []
+
+# for i in word_list:
+#     if word in i:
+#         target_list.append(i)
+# print(target_list)
+
+
+# a = input("Enter your text: ")
+
+# print(a.upper(),)
+# print(a.lower())
+
+# ll = ["This","is","python"]
+# largest = ll[0]
+
+# for i in ll:
+#     if len(i) > len(largest):
+#         largest = i
+# print(i)
+
+
+
+
+# sentence = "The Quick Brown Fox"
+# sentence = sentence.replace(" ", "")
+# y = 0
+# z = 0
+
+# for i in sentence:
+#     if i.isupper():
+#         y += 1
+#     else:
+#         z += 1
+# print(y, z)
+
